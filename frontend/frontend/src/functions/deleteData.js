a43b0882699cbe5466e61f0fa7e8db9befce9b69
James Henderson <james@identyti.io>
2020-02-10 15:16:35 -0500

    refactor http request methods

--- frontend/frontend/src/functions/deleteData.js
+++ frontend/frontend/src/functions/deleteData.js
@@ -1,7 +1,7 @@
 export default async function getData(url = '', queryParams = '') {
 // Default options are marked with *
   const response = await fetch(url+queryParams, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
--- frontend/frontend/src/functions/linkedInLogin.js
+++ frontend/frontend/src/functions/linkedInLogin.js
@@ -1,4 +1,7 @@
+import postData from './postData';
+
+
 export default function logIn() {
   console.log("Sign in implementation");
-
+  
 }
