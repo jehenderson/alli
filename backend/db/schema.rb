3aafc8fe3b87de5547caed658b6728da024fdaa0
James Henderson <james@identyti.io>
2020-02-11 14:18:40 -0500

    apply supporter migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200211040406) do
+ActiveRecord::Schema.define(version: 20200211191634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20200211040406) do
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
     t.index ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+  end
+
+  create_table "supporters", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_picture"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
