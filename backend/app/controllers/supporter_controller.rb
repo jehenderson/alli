99f4d1939fdc646870e86b2454e7bcdad1c3e9cc
James Henderson <james@identyti.io>
2020-02-11 14:22:19 -0500

    generate supporter controller, apply CRUD & params

---
+++ backend/app/controllers/supporter_controller.rb
@@ -0,0 +1,31 @@
+class SupporterController < ApplicationController
+  def index
+    @cards = Supporter.all
+    render json: @cards, status: :ok
+  end
+
+  def create
+    @card = Supporter.new(support_params)
+    @card.save
+    render json: @card, status: :created
+  end
+
+  def show
+    @card = Supporter.where(id: params[:id]).first
+    render json: @card, status: :ok
+  end
+
+  def destroy
+    @card = Supporter.where(id: params[:id]).first
+    if @card.destroy
+      head(:ok)
+    else head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def support_params
+    params.require(:supporter).permit(:first_name, :last_name, :profile_picture, :email)
+  end
+end
